564ecad2b6d05bc191ef8d2424d4224bea8ecaf1
LI-YONG-QI <a2369028@gmail.com>
2025-02-17 17:56:20 +0800

    Revert "chore: bucket swagger"
    This reverts commit 281e2a40ef2d4d2abe71a9e5d97c9e4e3cf9d693.

--- src/bucket/bucket.controller.ts
+++ src/bucket/bucket.controller.ts
@@ -1,15 +1,11 @@
 import { Controller, Get, Query } from '@nestjs/common';
 import { BucketService } from './bucket.service';
-import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
 @Controller('bucket')
-@ApiTags('bucket')
 export class BucketController {
   constructor(private readonly bucketService: BucketService) {}
 
   @Get('bottles')
-  @ApiOperation({ summary: 'Get user bottles' })
-  @ApiResponse({ status: 200, description: 'Get user bottles' })
   getBottles(@Query('address') address: string) {
     return this.bucketService.getBottles(address);
   }
